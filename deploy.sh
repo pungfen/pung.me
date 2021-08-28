e4f41a94eb8467e185b5e6e45619636d6d9fda05
pung <fp94214@live.com>
2021-08-28 15:11:12 +0800

    ~

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,23 @@
-#!/usr/bin/env sh
-
-# abort on errors
+# 发生任何错误时终止
 set -e
 
-# build
-yarn build
+# 构建
+npm run build
 
-# navigate into the build output directory
+# 进入输出产物文件夹
 cd docs/.vitepress/dist
 
-# if you are deploying to a custom domain
+# 如果你要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
+# 如果你要部署在 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-git push -f git@github.com:pungfen/pung.me.git main:gh-pages
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:pungfen/pung.me.gitt main:gh-pages
 
 cd -
