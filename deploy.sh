6012ad839d645d3cb1b199de8922d3e8d6cc874a
pung <fp94214@live.com>
2021-08-28 15:18:40 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:pungfen/pung.me.gitt main:gh-pages
+git push -f git@github.com:pungfen/pung.me.git main:gh-pages
 
 cd -
